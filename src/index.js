cba2589346b3fdb687c40b9a6dd851666523f3e7
Jayaram Nair <jayaramsivaramannair@gmail.com>
2022-03-02 04:35:29 -0500

    Created a new Link component to display the link's description and url

--- src/index.js
+++ src/index.js
@@ -2,16 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+
+
+//1. Import the required components from @apollo/client package
+import {
+  ApolloProvider, 
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
+
+
+//2. Expose the server link where the GraphQL server will be running
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+})
+
+
+//3. Create an instance of an ApolloClient
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
