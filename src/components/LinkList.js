f72702facc0696bcf3b0be517d93b72a996e60a0
Jayaram Nair <jayaramsivaramannair@gmail.com>
2022-03-06 07:13:38 -0500

    Enabled GraphQL subscription in LinkList component which updates the app in realtime when new links are created

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -4,6 +4,27 @@ import Link from './Link';
 //Imports useQuery hook from @apollo/client package
 import { useQuery, gql } from '@apollo/client';
 
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      id
+      url
+      description
+      createdAt
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
+    }
+  }
+`;
+
 //A query is sent to the server using gql query string and then the resultant data is stored in FEED_QUERY
 export const FEED_QUERY = gql`
   {
@@ -31,7 +52,33 @@ export const FEED_QUERY = gql`
 
 const LinkList = () => {
   //The data returned from the graphQL server is extracted so that it can be used to display links
-  const {data} = useQuery(FEED_QUERY)
+  const {
+    data,
+    loading,
+    error,
+    subscribeToMore
+  } = 
+  useQuery(FEED_QUERY)
+
+  subscribeToMore({
+    document: NEW_LINKS_SUBSCRIPTION,
+    updateQuery: (prev, { subscriptionData }) => {
+      if(!subscriptionData.data) return prev;
+      const newLink = subscriptionData.data.newLink;
+      const exists = prev.feed.links.find(
+        ({id}) => id === newLink.id
+      );
+      if (exists) return prev;
+
+      return Object.assign({}, prev, {
+        feed: {
+          links: [newLink, ...prev.feed.links],
+          count: prev.feed.links.length + 1,
+          __typename: prev.feed.__typename
+        }
+      })
+    }
+  })
 
   return (
     <div>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import {setContext} from '@apollo/client/link/context';
+import {split} from '@apollo/client';
+import {WebSocketLink} from '@apollo/client/link/ws';
+import {getMainDefinition} from '@apollo/client/utilities';
 import './styles/index.css';
 import App from './components/App';
 
@@ -22,6 +25,17 @@ const httpLink = createHttpLink({
 })
 
 
+//Create a WebSocketLink to represent the WebSocket connection
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN)
+    }
+  }
+})
+
 //Setting token obtained from local storage to authorize user requests by using the token
 const authLink = setContext((_, {headers}) => {
   const token = localStorage.getItem(AUTH_TOKEN)
@@ -33,10 +47,27 @@ const authLink = setContext((_, {headers}) => {
   }
 })
 
+//Using split to properly route the requests
+//Split function takes in three arguments:
+// The first is a test function which returns a boolean
+// The second argument is the link where the request will be forwarded to if the function returns true
+// the third argument is the link where the request will be forwarded to if the first argument returns false
+const link = split(
+  ({query}) => {
+    const {kind, operation} = getMainDefinition(query);
+    return (
+      kind === 'OperationDefinition' &&
+      operation === 'subscription'
+    );
+  },
+  wsLink,
+  authLink.concat(httpLink)
+)
+
 
 //3. Create an instance of an ApolloClient
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache()
 })
 
